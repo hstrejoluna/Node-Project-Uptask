623f1a05921fa4abfb6e64255fbab5658f2d192d
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-18 18:03:47 -0500

    express-validator imported and workin'

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const projectsController = require("../controllers/projectsController");
+// import express validator
+const { body } = require("express-validator/check");
+
 
 
 module.exports = function () {
   router.get("/", projectsController.projectHome);
   router.get("/new-project", projectsController.formProject);
-  router.post("/new-project", projectsController.newProject);
+  router.post("/new-project",
+  body('name').not().isEmpty().trim().escape(),
+  projectsController.newProject);
 
   return router;
 };
