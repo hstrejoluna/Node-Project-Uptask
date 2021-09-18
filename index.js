044b9c58a4cab3a71a0d44ea43256ae795b01d0a
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-18 13:14:54 -0500

    pug added and public resources

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
-
+const path = require("path");
 
 app.listen(3000, () => console.log("Server started on port 3000"));
+
+//Enable Pug
+app.set("view engine", "pug");
+
+// Add view folder
+app.set("views", path.join(__dirname, "./views"));
 
 
 app.use("/", routes());
