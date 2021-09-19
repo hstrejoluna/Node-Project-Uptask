e160a17f51eb1feebf02d8a2b755401b00d1c68f
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-19 14:24:03 -0500

    swal for delete button

--- public/js/modules/projects.js
+++ public/js/modules/projects.js
@@ -1,8 +1,29 @@
 import Swal from "sweetalert2";
 import axios from "axios";
 
-const btnDelete = document.querySelectorAll("#delete-project");
+const btnDelete = document.querySelector("#delete-project");
 
-btnDelete.addEventListener("click", () => {
-  console.log("you're clickin' on delete");
-});
+if (btnDelete) {
+  btnDelete.addEventListener("click", () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire("Deleted!", "Your project has been deleted.", "success");
+      }
+    });
+
+    // redirect to projects page
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 3000);
+  });
+}
+
+export default btnDelete;
