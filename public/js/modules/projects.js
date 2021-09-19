e128c5e1a60ce36643304ef7a94b7d62ec926565
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-19 14:38:12 -0500

    axios config

--- public/js/modules/projects.js
+++ public/js/modules/projects.js
@@ -4,7 +4,8 @@ import axios from "axios";
 const btnDelete = document.querySelector("#delete-project");
 
 if (btnDelete) {
-  btnDelete.addEventListener("click", () => {
+  btnDelete.addEventListener("click", (e) => {
+    const urlProject = e.target.dataset.projectUrl;
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -15,6 +16,11 @@ if (btnDelete) {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.value) {
+        // Send request to axios
+        const url = `${location.origin}/projects/${urlProject}`;
+        axios.delete(url, { params: urlProject }).then(function (response) {
+          console.log(response);
+        });
         Swal.fire("Deleted!", "Your project has been deleted.", "success");
       }
     });
