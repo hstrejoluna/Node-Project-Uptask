e8de9ad4bd563a2a1ad02ba928e5fe8473baccd3
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-19 14:02:35 -0500

    axios install, fixed layout

---
+++ public/js/modules/projects.js
@@ -0,0 +1,8 @@
+import Swal from "sweetalert2";
+import axios from "axios";
+
+const btnDelete = document.querySelectorAll("#delete-project");
+
+btnDelete.addEventListener("click", () => {
+  console.log("you're clickin' on delete");
+});
