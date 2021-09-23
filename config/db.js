5c21fda6d25b67af8c9bf8566ae17a8e13b6364d
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-23 18:29:39 -0500

    env for localhost

--- config/db.js
+++ config/db.js
@@ -1,22 +1,27 @@
 const Sequelize = require("sequelize");
-
-const db = new Sequelize("uptasknode", "root", "osoregordete", {
-  host: "127.0.0.1",
-  port: 3306,
-  dialect: "mariadb",
-  operatorsAliases: 0,
-  define: {
-    timestamps: 0,
-  },
-  dialectOptions: {
-    socketPath: "/var/run/mysqld/mysqld.sock",
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+require("dotenv").config({ path: "vars.env" });
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mariadb",
+    operatorsAliases: 0,
+    define: {
+      timestamps: 0,
+    },
+    dialectOptions: {
+      socketPath: "/var/run/mysqld/mysqld.sock",
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 module.exports = db;
