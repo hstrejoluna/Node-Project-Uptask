acc42ccc1b182cc734a5d13cebe296a78028dfb2
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-23 11:16:33 -0500

    verify that account exists

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,31 +1,32 @@
-const Users = require("../models/Users");
+const passport = require("passport");
+const Users = require("../models/user");
 
 // form signup
 
-exports.formSignup = (req, res) => {
+export function formSignup(req, res) {
   res.render("signUp", {
     pageName: "Sign Up",
   });
-};
+}
 
 //form login
 
-exports.formLogin = (req, res) => {
+export function formLogin(req, res) {
   const { error } = res.locals.messages;
   res.render("login", {
     pageName: "Login",
     error,
   });
-};
+}
 
 //Sign up
 
-exports.signUp = async (req, res) => {
+export async function signUp(req, res) {
   //read data
   const { email, password } = req.body;
 
   try {
-    await Users.create({
+    await create({
       email,
       password,
     });
@@ -42,10 +43,19 @@ exports.signUp = async (req, res) => {
       password,
     });
   }
-};
+}
 
-exports.formResetpassword = (req, res) => {
+export function formResetpassword(req, res) {
   res.render("resetPassword", {
     pageName: "Reset Password",
   });
+}
+
+exports.sendToken = async (req, res) => {
+  const { email } = req.body;
+  const user = await Users.findOne({ email });
+  if (!user) {
+    req.flash("error", "No account with that email exists");
+    return res.redirect("/reset");
+  }
 };
