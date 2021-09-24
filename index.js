ea9737e9ed2bf0544b2d2ba9939007b367fe0974
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-23 19:49:06 -0500

    port

--- index.js
+++ index.js
@@ -75,7 +75,4 @@ const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
-});
-
+app.listen(process.env.PORT || 3000);
